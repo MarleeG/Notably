362921b872bfd4c5c8726d4820a8ec10b5805977
Marlee Gerard <marleegerard@gmail.com>
2021-05-31 22:36:40 -0400

    added the application note up at the top of the UI.

---
+++ client/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ApplicationNote from "./ApplicationNote";
+
+const Main = () => {
+  return (
+    <div>
+      <ApplicationNote />
+    </div>
+  );
+};
+
+export default Main;
